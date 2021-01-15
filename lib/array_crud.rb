8ffedd440ed3d679aa3550eb3b75a410e5e9623d
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-15 20:23:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
 array = ["I", "am", "really", "learning", "arrays!"]
-array.pop 
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
@@ -28,7 +28,8 @@ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array [2]
 end
 
 def retrieve_first_element_from_array(array)
